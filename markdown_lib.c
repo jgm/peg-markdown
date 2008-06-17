fb8d07d70bc1b64f3acfbd3b57f3c1d10abd073f
John MacFarlane <jgm@berkeley.edu>
2008-06-17 09:43:01 -0700

    Added comment header to markdown_lib.c.

--- markdown_lib.c
+++ markdown_lib.c
@@ -1,3 +1,19 @@
+/**********************************************************************
+
+  markdown_lib.c - markdown in C using a PEG grammar.
+  (c) 2008 John MacFarlane (jgm at berkeley dot edu).
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; version 2 of the License.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+ ***********************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
