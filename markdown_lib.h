29862b258f6ba0772c9eb23911273a17d97dcbfb
rekado <rekado@elephly.net>
2012-10-27 20:13:02 +0800

    New extension: autolink for converting bare URLs to links

--- markdown_lib.h
+++ markdown_lib.h
@@ -7,7 +7,8 @@ enum markdown_extensions {
     EXT_NOTES            = 0x02,
     EXT_FILTER_HTML      = 0x04,
     EXT_FILTER_STYLES    = 0x08,
-    EXT_STRIKE           = 0x10
+    EXT_STRIKE           = 0x10,
+    EXT_AUTOLINK         = 0x20
 };
 
 enum markdown_formats {
